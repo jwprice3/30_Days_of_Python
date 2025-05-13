16ef38c5628f70a2d34679bcf6c1e5729bcfc4af
jwprice3 <justin.w.price1@outlook.com>
2025-05-13 13:54:53 -0700

    Add files via upload
    Day 03 of 30 days of python

--- Day_3_Exercise.py
+++ Day_3_Exercise.py
@@ -54,70 +54,133 @@ The perimeter of the triangle is 12
     4 1 4 16 64
     5 1 5 25 125
 '''
-my_age = 30
-my_height = 6.1
-my_complex_number = 3 + 4j
+# my_age = 30
+# my_height = 6.1
+# my_complex_number = 3 + 4j
 
-#Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h)
-print('Find the area of a tringle')
-print('Enter Base:')
-base = input()
-print('Enter Height')
-height = input()
-area_of_trianle = .5 * int(base) * int(height) #this will throw an error unless you specify what the variable will be ex. int(base)
-print('The area of the triangle is', area_of_trianle)
+# #Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h)
+# print('Find the area of a tringle')
+# print('Enter Base:')
+# base = input()
+# print('Enter Height')
+# height = input()
+# area_of_trianle = .5 * int(base) * int(height) #this will throw an error unless you specify what the variable will be ex. int(base)
+# print('The area of the triangle is', area_of_trianle)
 
-#Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
-print('Enter side a:')
-side_a = input()
-print('Enter side b:')
-side_b = input()
-print('Enter side c:')
-side_c = input()
-perimeter_of_triangle = int(side_a) + int(side_b) + int(side_c)
-print('The perimeter of the triangle is', int(perimeter_of_triangle))
+# #Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+# print('Enter side a:')
+# side_a = input()
+# print('Enter side b:')
+# side_b = input()
+# print('Enter side c:')
+# side_c = input()
+# perimeter_of_triangle = int(side_a) + int(side_b) + int(side_c)
+# print('The perimeter of the triangle is', int(perimeter_of_triangle))
 
-#Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
-print('Find the area and perimeter of a rectangle.')
-print('Enter side a:')
-side_a_w = input()
-print('Enter side b:')
-side_b_l = input()
-area_of_rectangle = int(side_a_w) * int(side_b_l)
-perimeter_of_rectangle = (int(side_a_w) + int(side_b_l)) * 2
-print('The area of the rectangle is', int(area_of_rectangle))
-print('The perimeter of a rectangle is', int(perimeter_of_rectangle))
+# #Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+# print('Find the area and perimeter of a rectangle.')
+# print('Enter side a:')
+# side_a_w = input()
+# print('Enter side b:')
+# side_b_l = input()
+# area_of_rectangle = int(side_a_w) * int(side_b_l)
+# perimeter_of_rectangle = (int(side_a_w) + int(side_b_l)) * 2
+# print('The area of the rectangle is', int(area_of_rectangle))
+# print('The perimeter of a rectangle is', int(perimeter_of_rectangle))
 
-#Get radius of a circle using prom1
-#Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
-print('Get the radius and circumference of a circle')
-print('Enter Radius:')
-raidus = input()
-area_of_a_cirlce = 3.14 * int(raidus)**2
-circumference = 2 * 3.14 * int(raidus)
-print('Get the radius and circumference of a circle')
-print('Enter Radius:', int(raidus))
-print('Area is', int(area_of_a_cirlce))
-print('Circrumference is', int(circumference))
+# #Get radius of a circle using prom1
+# #Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+# print('Get the radius and circumference of a circle')
+# print('Enter Radius:')
+# raidus = input()
+# area_of_a_cirlce = 3.14 * int(raidus)**2
+# circumference = 2 * 3.14 * int(raidus)
+# print('Get the radius and circumference of a circle')
+# print('Enter Radius:', int(raidus))
+# print('Area is', int(area_of_a_cirlce))
+# print('Circrumference is', int(circumference))
 
-#Calculate the slope, x-intercept and y-intercept of y = 2x -2
-print('Find the slope')
-x = input()
-y_slope = (2*int(x)-2)
-print(y_slope)
+# #Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# print('Find the slope')
+# print('Enter x:', )
+# x = input()
+# y_slope = (2*int(x)-2)
+# print(y_slope)
 
-#Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
-print('Point one is (2,2)')
-print('Point one is (6,10)')
-slope = (10 - 2) / (6 - 2)
-print(slope)
+# #Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+# print('Point one is (2,2)')
+# print('Point one is (6,10)')
+# slope = (10 - 2) / (6 - 2)
+# print(slope)
 
-#Compare the slopes in tasks 8 and 9.
-print('y slope is', y_slope, 'and', 'slope is', slope)
+# #Compare the slopes in tasks 8 and 9.
+# print('y slope is', y_slope, 'and', 'slope is', slope)
 
-#Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
-print('Enter x:', )
-x = input()
-print(int(x)**2)
-value_of_y = int(x)**2 + 6*(int(x)) + 9
-#print(value_of_y)
+# #Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+# print('Enter x:', )
+# x = input()
+# print(int(x)**2)
+# value_of_y = int(x)**2 + 6*(int(x)) + 9
+# print('y =',value_of_y)
+
+# Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+# python = 'python'
+# dragon = 'dragon'
+# print(len(python) != len(dragon))
+
+# Use and operator to check if 'on' is found in both 'python' and 'dragon'
+# print('Is on is in python?', 'on' in ('python' and 'dragon'))
+# print('Is on is in  dragon?', 'on' in 'dragon')
+# print('True - on is found in dragon and python.')
+
+# I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+# print('Is jargon in this sentence? I hope this course is not full of jargon', 'jargon' in 'I hope this course is not full of jargon')
+
+# There is no 'on' in both dragon and python
+# print('Is there <no> in python and dragon?', 'no' in (python and dragon))
+
+# Find the length of the text python and convert the value to float and convert it to string
+# print("What is length of python?")
+# print(len(python))
+# print(float(len(python)))
+# python = str(float(len(python)))
+# print(type(python))
+
+# Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+# y = 2
+# print("Is this number divisible by 2?:")
+# x = int(input())
+# print("The number is", x)
+# print((x/2) == (x//2))
+
+# Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+# print("Is 7//2 equal to 2.7?", "Answer:", (7//2) == (2.7))
+
+# Check if type of '10' is equal to type of 10
+# print("Q: Is type of '10' is equal to type of 10?", type("10") == type(10))
+
+# Check if int('9.8') is equal to 10
+# print("Q: Check if int('9.8') is equal to 10", int(9.8) == 10)
+
+# Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+# print("Q: Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?")
+# hours = int(input())
+# rate_per_hour = 28
+# weekly_earning = hours * rate_per_hour
+
+# print("Please enter the amount the worked.")
+# print("Enter the amount of hours worked.", hours)
+# print("Your hourly rate is:", "$", rate_per_hour)
+# print("Weekly total is:", "$", weekly_earning, "Uncle Sam withstanding.")
+
+# Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+print("Q: Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years")
+print("How many years have you lived?","Enter a number between 0 - 100.")
+age = int(input())
+print("The user is",age, "years old")
+seconds_lived = age * 31536000
+print("You have lived",seconds_lived,"seconds.")
+
+# Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+print("Write a Python script that displays the following table")
+print("1 1 1 1 1\n""2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
--- Day_3_Notes.py
+++ Day_3_Notes.py
@@ -24,100 +24,100 @@ Assignment Operators: Operators are used to assign values to variables.
 Comparison operator: Operators to compare two values in True or False. Can be by symbol, word, or logic
 '''
 
-# Arithmetic Operations in Python
-# Integers
-print('Addition: ', 1 + 2)        # 3
-print('Subtraction: ', 2 - 1)     # 1
-print('Multiplication: ', 2 * 3)  # 6
-print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
-print('Division: ', 6 / 2)        # 3.0         
-print('Division: ', 7 / 2)        # 3.5
-print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
-print ('Division without the remainder: ',7 // 3)   # 2
-print('Modulus: ', 3 % 2)         # 1, Gives the remainder
-print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
+# # Arithmetic Operations in Python
+# # Integers
+# print('Addition: ', 1 + 2)        # 3
+# print('Subtraction: ', 2 - 1)     # 1
+# print('Multiplication: ', 2 * 3)  # 6
+# print ('Division: ', 4 / 2)       # 2.0  Division in Python gives floating number
+# print('Division: ', 6 / 2)        # 3.0         
+# print('Division: ', 7 / 2)        # 3.5
+# print('Division without the remainder: ', 7 // 2)   # 3,  gives without the floating number or without the remaining
+# print ('Division without the remainder: ',7 // 3)   # 2
+# print('Modulus: ', 3 % 2)         # 1, Gives the remainder
+# print('Exponentiation: ', 2 ** 3) # 9 it means 2 * 2 * 2
 
-# Floating numbers
-print('Floating Point Number, PI', 3.14)
-print('Floating Point Number, gravity', 9.81)
+# # Floating numbers
+# print('Floating Point Number, PI', 3.14)
+# print('Floating Point Number, gravity', 9.81)
 
-# Complex numbers
-print('Complex number: ', 1 + 1j)
-print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+# # Complex numbers
+# print('Complex number: ', 1 + 1j)
+# print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
 
-#PRACTICE
-# Declaring the variable at the top first
-a = 3 #interger data type
-b = 2 #interger data type
+# #PRACTICE
+# # Declaring the variable at the top first
+# a = 3 #interger data type
+# b = 2 #interger data type
 
-#Assigning the result of a variable #Try to label your outcome
-total = a + b
-diff = a-b
-product = a * b
-division = a/b
-remainder = a %b
-floor_division = a//b
-exponentiation = a**b
+# #Assigning the result of a variable #Try to label your outcome
+# total = a + b
+# diff = a-b
+# product = a * b
+# division = a/b
+# remainder = a %b
+# floor_division = a//b
+# exponentiation = a**b
 
 
-print(total)
-print('a + b = ', total)
-print('a - b = ', diff)
-print('a * b = ', product)
-print('a / b = ', division)
-print('a % b = ', remainder)
-print('a // b = ', floor_division)
-print('a + b = ', exponentiation)
+# print(total)
+# print('a + b = ', total)
+# print('a - b = ', diff)
+# print('a * b = ', product)
+# print('a / b = ', division)
+# print('a % b = ', remainder)
+# print('a // b = ', floor_division)
+# print('a + b = ', exponentiation)
 
-#Declaring Variables and Organizing
-num_one = 3
-num_two = 4
+# #Declaring Variables and Organizing
+# num_one = 3
+# num_two = 4
 
-total = num_one + num_two
-diff = num_two - num_one
-product = num_one * num_two
-div = num_two / num_one
+# total = num_one + num_two
+# diff = num_two - num_one
+# product = num_one * num_two
+# div = num_two / num_one
 
-print('total', total)   # having a period after a string ex <'total' . total> throws an error 
-print ('difference', diff)
-print('product', product)
-print('division', div)
+# print('total', total)   # having a period after a string ex <'total' . total> throws an error 
+# print ('difference', diff)
+# print('product', product)
+# print('division', div)
 
-#example | radius
-radius = 10
-area_of_circle = 3.14 * radius ** 2
-print('Area of a circle:', area_of_circle)
+# #example | radius
+# radius = 10
+# area_of_circle = 3.14 * radius ** 2
+# print('Area of a circle:', area_of_circle)
 
-#example | area of a rectangle
-length = 10
-width = 20
-area_of_rectangle = length * width
-print('Area of rectangle:', area_of_rectangle)
+# #example | area of a rectangle
+# length = 10
+# width = 20
+# area_of_rectangle = length * width
+# print('Area of rectangle:', area_of_rectangle)
 
-#Comparison
-print(3 > 2)     # True, because 3 is greater than 2
-print(3 >= 2)    # True, because 3 is greater than 2
-print(3 < 2)     # False,  because 3 is greater than 2
-print(2 < 3)     # True, because 2 is less than 3
-print(2 <= 3)    # True, because 2 is less than 3
-print(3 == 2)    # False, because 3 is not equal to 2
-print(3 != 2)    # True, because 3 is not equal to 2
-print(len('mango') == len('avocado'))  # False, "len" compares the character amount
-print(len('mango') != len('avocado'))  # True,  "len" compares the character amount
-print(len('mango') < len('avocado'))   # True, "len" compares the character amount
-print(len('milk') != len('meat'))      # False, "len" compares the character amount
-print(len('milk') == len('meat'))      # True, "len" compares the character amount
-print(len('tomato') == len('potato'))  # True, "len" compares the character amount
-print(len('python') > len('dragon'))   # False, "len" compares the character amount
+# #Comparison
+# print(3 > 2)     # True, because 3 is greater than 2
+# print(3 >= 2)    # True, because 3 is greater than 2
+# print(3 < 2)     # False,  because 3 is greater than 2
+# print(2 < 3)     # True, because 2 is less than 3
+# print(2 <= 3)    # True, because 2 is less than 3
+# print(3 == 2)    # False, because 3 is not equal to 2
+# print(3 != 2)    # True, because 3 is not equal to 2
+# print(len('mango') == len('avocado'))  # False, "len" compares the character amount
+# print(len('mango') != len('avocado'))  # True,  "len" compares the character amount
+# print(len('mango') < len('avocado'))   # True, "len" compares the character amount
+# print(len('milk') != len('meat'))      # False, "len" compares the character amount
+# print(len('milk') == len('meat'))      # True, "len" compares the character amount
+# print(len('tomato') == len('potato'))  # True, "len" compares the character amount
+# print(len('python') > len('dragon'))   # False, "len" compares the character amount
 
-# Comparing something gives either a True or False
+# # Comparing something gives either a True or False
 
-print('True == True: ', True == True)
-print('True == False: ', True == False)
-print('False == False:', False == False)
+# print('True == True: ', True == True)
+# print('True == False: ', True == False)
+# print('False == False:', False == False)
 
-# Word operators are also True or False
-#is: Returns true if both variables are the same object(x is y)
+# # Word operators are also True or False
+# #is: Returns true if both variables are the same object(x is y)
 print('1 is 1', 1 is 1)
 print('True - because the data values are the same')
 #is not: Returns true if both variables are not the same object(x is not y)
@@ -137,40 +137,40 @@ print('True - the product of of 2**2 is equal to 4')
 print('5 is 2 ** 2:', 5 is 2 ** 2) 
 print('False - the product of of 2**2 is not equal to 5')
 
-#logical operators still True of False questions
-print(3 > 2 and 4 > 3)
-print('True - because both statements are true, making 2 out of 2 factually correct statements')
-print(3 > 2 and 4 < 3)
-print('False - because the second statement is false, making 1 out of 2 factually incorrect statements')
-print(3 < 2 and 4 < 3)
-print('False - because both statements are false, making 2 out of 2 factually incorrect statements')
-print('True and True: ', True and True)
-print('True - because True and True are the same')
-x = 5
-print(5 > 4 and x > 3)
-print('True - becuase the value of 5 and x are greater than 4, making 2 out of 2 factually correct statements')
-print(3 > 2 or 4 > 3)
-print('True - because both statements are true, making 2 out of 2 factually correct statements')
-print(3 > 2 or 4 < 3)
-print('True - because one of the statements is true, meeting the requirement of 1 out of 2 factually correct statements')
-print(3 < 2 or 4 < 3)  # 
-print('False - because both statements are false , not meeting the requirement of 1 out of 2 factually correct statement')
-print('True or False:', True or False)
-print('True - because one of the statements is the value of true, meeting the requirement of 1 out of 2 factually correct statements')
+# #logical operators still True of False questions
+# print(3 > 2 and 4 > 3)
+# print('True - because both statements are true, making 2 out of 2 factually correct statements')
+# print(3 > 2 and 4 < 3)
+# print('False - because the second statement is false, making 1 out of 2 factually incorrect statements')
+# print(3 < 2 and 4 < 3)
+# print('False - because both statements are false, making 2 out of 2 factually incorrect statements')
+# print('True and True: ', True and True)
+# print('True - because True and True are the same')
+# x = 5
+# print(5 > 4 and x > 3)
+# print('True - becuase the value of 5 and x are greater than 4, making 2 out of 2 factually correct statements')
+# print(3 > 2 or 4 > 3)
+# print('True - because both statements are true, making 2 out of 2 factually correct statements')
+# print(3 > 2 or 4 < 3)
+# print('True - because one of the statements is true, meeting the requirement of 1 out of 2 factually correct statements')
+# print(3 < 2 or 4 < 3)  # 
+# print('False - because both statements are false , not meeting the requirement of 1 out of 2 factually correct statement')
+# print('True or False:', True or False)
+# print('True - because one of the statements is the value of true, meeting the requirement of 1 out of 2 factually correct statements')
 
-#Not examples | pretty much confirms yes and no
-print(not 3 > 2)
-print('Lawyers speak like this, is it NOT true that 3 is bigger than 2?” Since 3 is bigger, the answer is “No, it’s not NOT true,')
-print(not True)
-print('False - Negation, the not operator turns true to false')
-print(not False)
-print('True - is printing the opposite of False')
-print(not not True)  # True
-print('True - If I say that I am NOT, not mad at you, that means I am mad bruv')
-print(not not False) # False
-print('False - Start with False ("no"). First not: not False = True ("yes"). Second not: not True = False ("no"). So, print(not not False) prints False.')
-print('Here is a real world example:')
-print('I am building a toy robot, my robot will rust in the rain.')
-print('Rain = True if "not raining"  if it is False for "not raining" (robot stays still).')
-print('No rain = False if "not raining" if it is True "not raining" (robot moves!).') 
-print('In code, it might look like: move_robot(not is_raining)')
+# #Not examples | pretty much confirms yes and no
+# print(not 3 > 2)
+# print('Lawyers speak like this, is it NOT true that 3 is bigger than 2?” Since 3 is bigger, the answer is “No, it’s not NOT true,')
+# print(not True)
+# print('False - Negation, the not operator turns true to false')
+# print(not False)
+# print('True - is printing the opposite of False')
+# print(not not True)  # True
+# print('True - If I say that I am NOT, not mad at you, that means I am mad bruv')
+# print(not not False) # False
+# print('False - Start with False ("no"). First not: not False = True ("yes"). Second not: not True = False ("no"). So, print(not not False) prints False.')
+# print('Here is a real world example:')
+# print('I am building a toy robot, my robot will rust in the rain.')
+# print('Rain = True if "not raining"  if it is False for "not raining" (robot stays still).')
+# print('No rain = False if "not raining" if it is True "not raining" (robot moves!).') 
+# print('In code, it might look like: move_robot(not is_raining)')
