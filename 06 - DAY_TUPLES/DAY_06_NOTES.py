e05d9061947ab318776c163297b69fd165751b6d
jwprice3 <justin.w.price1@outlook.com>
2025-07-18 13:29:21 -0700

    Add files via upload

---
+++ 06 - DAY_TUPLES/DAY_06_NOTES.py
@@ -0,0 +1,26 @@
+################################
+# Title: 30 Days of Python
+# Description: Tuples
+# Change Log:
+# JP, Created, 7.17.25
+################################
+
+# 4 types of Data types
+    # List
+    # Tuple
+        # A collection which is ordered and unchangeable or unmodifiable(immutable). 
+        # Allows duplicate members.
+        # Related to count()
+        # Related to index()
+    # Set
+    # Dictionary
+
+# How to create a tuple
+    # Tuple are represented by ()
+    # entries in a list a seperated by a , (comma)
+
+print('Begin: What determines a Tuple.')
+# <variable> = () or <variable> = tuple()
+empty_tuple = ()
+empty_tuple = tuple()
+print('End: What determines a Tuple.')
