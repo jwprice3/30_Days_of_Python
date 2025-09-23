a8d698f9972e79fded6c61b0150cc76e1f592b7c
jwprice3 <justin.w.price1@outlook.com>
2025-09-23 15:49:40 -0700

    Add files via upload

---
+++ 08 - DAY_DICTIONARIES/DAY_08_DICT_EXERCISES.py
@@ -0,0 +1,54 @@
+################################
+# Title: 30 Days of Python
+# Description: Dictionaries | Exercices
+# Change Log:
+# JP, Created, 9.23.25
+################################
+
+
+print('Question 1: Create an empty dictionary called dog.')
+dog = {}
+print(type(dog))
+
+print('Question 2: Add name, color, breed, legs, age to the dog dictionary.')
+dog = {'name':'Bruno','breed':'Husky','legs':'4','age':'3'}
+print(dog)
+
+print('Question 3: Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary.')
+student = {'first_name':'Steve','last_name':'Rogers',
+           'gender':'male','age':'95','marital status':'singlish',
+           'skills':'leadership ,hand to hand, super strength',
+           'country':'USA','city':'Brooklyn','address':'Avengers Tower'}
+print(student)
+
+print('Question 4: Get the length of the student dictionary.')
+print(len(student)) # note it counts the amount of vaules
+
+print('Question 5: Get the value of skills and check the data type, it should be a list.')
+print(student['skills'])
+# or
+print(student.get('skills'))
+
+print('Question 6: Modify the skills values by adding one or two skills.')
+student['skills'] = 'leadership ,hand to hand, super strength, tall'
+print(student)
+
+print('Question 7: Get the dictionary keys as a list.')
+student_keys = student.keys()
+print(student_keys)
+
+print('Question 8: Get the dictionary values as a list.')
+student_values = student.values()
+print(student_values)
+
+print('Question 9: Change the dictionary to a list of tuples using items() method.')
+student = {'first_name':'Steve','last_name':'Rogers',
+           'gender':'male','age':'95','marital status':'singlish',
+           'skills':'leadership ,hand to hand, super strength',
+           'country':'USA','city':'Brooklyn','address':'Avengers Tower'}
+student = list(student.items()) 
+print(student)
+print(type(student))
+
+
+
--- 08 - DAY_DICTIONARIES/DAY_08_NOTES_DICT.py
+++ 08 - DAY_DICTIONARIES/DAY_08_NOTES_DICT.py
@@ -8,7 +8,7 @@
 
 # A dictA dictionary is a collection of unordered, modifiable(mutable) paired (key: value) data type.
     # A dictionary is determined by {}
-    # Dictionary with keys and data values
+    # Dictionary has two components keys and values
     # dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
     # keys are normally strings as they are descriptors
     # person = { 'first_name':'Asabeneh', 'last_name':'Yetayeh','age':250, 'country':'Finland', 'is_marred':True, 'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'], 'address':{ 'street':'Space street', 'zipcode':'02210' }}
@@ -35,18 +35,26 @@ print(dct.get('key1'))
 print(dct.get('key5')) # does not result in error
 print('End: How determine if a key exist in a dictionary.')
 
-print('Begin: How to add a new key and value pair to a dictionary.') # variable/dct[<'key'>] = <value>
+print('Begin: How to add a key value pair to a dictionary.') # variable/dct[<'new key'>] = <new value>
 dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
 dct['key5'] = 'value5'
+print(dct)
 print('End: How to add a new key and value pair to a dictionary.')
 
-print('Begin: How determine if a key exist in a dictionary') # print(<'key'> in <variable/dct>)
+print('Begin: How to add a value to an prexisting key.') # variable/dct[<'key'>] = <value>
+# will overwrite previous value
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct['key5'] = 'value5'
+print(dct)
+print('End: How to add a value to an prexisting key.')
+
+print('Begin: How determine if a key exist in a dictionary.') # print(<'key'> in <variable/dct>)
 # booleon
 # operator
 dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
 print('key2' in dct)
 print('key5' in dct)
-print('End: How determine if a key exist in a dictionary')
+print('End: How determine if a key exist in a dictionary.')
 
 print('Begin: Multiple methods to remove key and value pairs from dictionary') # <variabel/dct>.pop('<key1>') <variabel/dct>.popitem() or del dct['key1']
 # method
@@ -65,4 +73,41 @@ print(dct)
 del dct['key2']
 print(dct)
 
+print('Begin: How to change from a dictionary to a list.') # <desired variable/list> = list(<variable/dictionary>.items())
+# method
+# .item()
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print(dct)
+print(type(dct))
+dct = list(dct.items())
+print(type(dct))
+print(dct)
+print('End: How to change from a dictionary to a list.')
 
+print('Begin: How to copy a dictionary.') # <variable/new dict> = <variable/origianl dict>.copy()
+# method
+# copy()
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+dct_copy = dct.copy()
+print(dct_copy)
+print('End: How to copy a dictionary.')
+
+print('Begin: How to get dictionary keys as a list.') # <varibale/ dict keys = <variable/dict>.keys()
+# method
+# .keys()
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+keys = dct.keys()
+print(keys)
+print(type(keys))
+print(keys)
+print('End: How to get dictionary keys as a list.')
+
+print('Begin: How to get dictionary values as a list.') # <varibale/ dict values = <variable/dict>.values()
+# method
+# .values()
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+values = dct.values()
+print(values)
+print(type(values))
+print(values)
+print('End: How to get dictionary vaules as a list.')
